c78ca0ce4fb026fb455674d9a753a8d184813cca
shaneshame <56442391+shaneshame@users.noreply.github.com>
2023-03-21 12:45:59 -0400

    Revert "Add Router (#8)"
    This reverts commit fadabc1171394f1236e9bb0de348c787cb65b48c.

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -2,38 +2,16 @@ import styled from 'styled-components';
 
 import colors from '../colors';
 
-const Container = styled.div`
+const ClickCounterContainer = styled.div`
   color: ${colors.grayUnselected};
   font-size: 2rem;
   font-weight: 500;
   width: 100%;
 `;
 
-const Clicks = styled.span`
-  color: ${(props) => (props.hasExceeded ? colors.red : colors.grayUnselected)};
+const ClickCounterSpan = styled.span`
+  color: ${(props) =>
+    props.hasExceeded ? colors.redSelected : colors.grayUnselected};
 `;
 
-const DELIMITER = '/';
-
-const ClickCounter = ({ clickedTiles = [], complexity, count, label }) => {
-  const numUniqueClicks = clickedTiles.filter(Boolean).length;
-
-  return (
-    <Container>
-      {label}
-      {complexity > 0 ? (
-        <>
-          <Clicks hasExceeded={count > complexity}>{count}</Clicks>
-          {DELIMITER}
-          {complexity}
-        </>
-      ) : (
-        numUniqueClicks
-      )}
-    </Container>
-  );
-};
-
-ClickCounter.displayName = 'ClickCounter';
-
-export { ClickCounter };
+export { ClickCounterContainer, ClickCounterSpan };
