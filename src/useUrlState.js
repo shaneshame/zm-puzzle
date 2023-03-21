58bd8cd1b759d2d0700501b9e7eda98e43f0eefc
Shane Schaefer <tf2shane@gmail.com>
2023-03-21 12:24:24 -0400

    Revert "Revert "Add Router (#8)""
    This reverts commit 59ff6c63b518d8d1a879095ab1b55658cdfcfd55.

---
+++ src/useUrlState.js
@@ -0,0 +1,87 @@
+import { useCallback, useMemo, useState } from 'react';
+import queryString from 'query-string';
+
+import { isFunction } from './util';
+
+const defaultParseOptions = {
+  arrayFormat: 'comma',
+  parseNumbers: true,
+  parseBooleans: true,
+};
+
+const defaultStringifyOptions = {
+  arrayFormat: 'comma',
+};
+
+const getOptions = (options = {}) => {
+  const { parseOptions = {}, stringifyOptions = {} } = options;
+
+  return {
+    parseOptions: {
+      ...defaultParseOptions,
+      ...parseOptions,
+    },
+    stringifyOptions: {
+      ...defaultStringifyOptions,
+      ...stringifyOptions,
+    },
+  };
+};
+
+const updateSearchParams = (query) => {
+  const url = new URL(`${window.location.origin}/?${query}`);
+
+  window.history.replaceState({}, '', url);
+};
+
+function useUrlState(initialState = {}, options = {}) {
+  const location = window.location;
+
+  const { parseOptions = {}, stringifyOptions = {} } = useMemo(() => {
+    return getOptions(options);
+  }, [options]);
+
+  const stateFromUrl = useMemo(() => {
+    return queryString.parse(location.search, parseOptions);
+  }, [location.search, parseOptions]);
+
+  const [currentState, setSearch] = useState(() => {
+    const init = isFunction(initialState) ? initialState() : initialState;
+
+    return {
+      ...init,
+      ...stateFromUrl,
+    };
+  });
+
+  const mergedState = useMemo(() => {
+    return {
+      ...currentState,
+      ...stateFromUrl,
+    };
+  }, [currentState, stateFromUrl]);
+
+  const setUrlState = useCallback(
+    (state) => {
+      setSearch((previousState) => {
+        const passedState = isFunction(state) ? state(mergedState) : state;
+
+        const newState = {
+          ...previousState,
+          ...passedState,
+        };
+
+        const newString = queryString.stringify(newState, stringifyOptions);
+
+        updateSearchParams(newString);
+
+        return newState;
+      });
+    },
+    [mergedState, stringifyOptions],
+  );
+
+  return [mergedState, setUrlState];
+}
+
+export default useUrlState;
