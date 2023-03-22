1f3ca35eaff2d05db05b6681e52ee6e3a6635420
shaneshame <56442391+shaneshame@users.noreply.github.com>
2023-03-22 16:02:40 -0400

    Update useUrlState Hook (#10)

---
+++ src/useUrlState/useUrlState.js
@@ -0,0 +1,76 @@
+import { useCallback, useMemo, useRef } from 'react';
+
+import { isFunction, queryString, updateUrlQuery } from './util';
+
+const setUrlQuery = (query) => {
+  const updatedUrl = updateUrlQuery(window.location.toString(), query);
+
+  window.history.replaceState({}, '', updatedUrl);
+};
+
+// Inspired by @remix react-router `useSearchParam` hook
+// https://github.com/remix-run/react-router/blob/590b7a25a454d998c83f4e5d6f00ad5a6217533b/packages/react-router-dom/index.tsx#L785
+
+function useUrlState(initialState = {}, options = {}) {
+  const location = window.location;
+
+  const serialize = useMemo(() => {
+    return options.serialize || queryString.stringify;
+  }, [options]);
+
+  const deserialize = useMemo(() => {
+    return options.deserialize || queryString.parse;
+  }, [options]);
+
+  const initialStateRef = useRef(
+    isFunction(initialState) ? initialState() : initialState,
+  );
+
+  const hasSetSearchParamsRef = useRef(false);
+
+  const urlState = useMemo(() => {
+    const currentUrlState = deserialize(location.search);
+
+    // Only merge in the defaults if we haven't yet called setSearchParams.
+    // Once we call that we want those to take precedence, otherwise you can't
+    // remove a param with setUrlState({}) if it has an initial value
+    const initState = hasSetSearchParamsRef.current
+      ? {}
+      : initialStateRef.current;
+
+    return {
+      ...initState,
+      ...currentUrlState,
+    };
+  }, [deserialize, location.search]);
+
+  if (!hasSetSearchParamsRef.current) {
+    const initialQuery = serialize(urlState);
+
+    setUrlQuery(initialQuery);
+  }
+
+  const setUrlState = useCallback(
+    (state) => {
+      hasSetSearchParamsRef.current = true;
+
+      const passedState = isFunction(state) ? state(urlState) : state;
+
+      const newState = {
+        ...urlState,
+        ...passedState,
+      };
+
+      const query = serialize(newState);
+
+      setUrlQuery(query);
+
+      return newState;
+    },
+    [serialize, urlState],
+  );
+
+  return [urlState, setUrlState];
+}
+
+export default useUrlState;
